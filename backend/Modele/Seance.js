1b69ed9240936c0f5c1c1bb9c9c45ff5407e1f3c
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-23 15:50:27 +0300

    modèle séance

--- backend/Modele/Seance.js
+++ backend/Modele/Seance.js
@@ -1,43 +1,58 @@
-const {DataTypes, Model} = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../database/database');
-const Module = require('./Module');
+const Formation = require('./formation'); // Assurez-vous d'importer Formation
+const Module = require('./Module'); // Assurez-vous d'importer Module
 
-class Seance extends Model{}
+class Seance extends Model {}
 
-Seance.init({
+Seance.init(
+    {
     date: {
-        type : DataTypes.DATE(),
+      type: DataTypes.DATE,
     },
-    heureStart:{
-        type : DataTypes.STRING(5), 
-        allowNull : false,
+    heureStart: {
+      type: DataTypes.STRING(5),
+      allowNull: false,
     },
-    heureEnd:{
-        type : DataTypes.STRING(5), 
-        allowNull : false,
+    heureEnd: {
+      type: DataTypes.STRING(5),
+      allowNull: false,
     },
-    formation:{
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        references : {
-        model : Formation,
-        key : 'id'
-    }},
-    module:{
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        references : {
-        model : Module,
-        key : 'id'
-    }},
-    nombreDePlaces:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-    }}
-,{
+    formation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Formation,
+        key: 'id',
+      },
+    },
+    module: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Module,
+        key: 'id',
+      },
+    },
+    nombreDePlaces: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
-    modelName : 'Module'
-})
+    modelName: 'Seance', 
+  }
+);
 
+Seance.belongsTo(Formation, {
+  foreignKey: 'formation',
+  onDelete: 'CASCADE',
+});
+
+Seance.belongsTo(Module, {
+  foreignKey: 'module',
+  onDelete: 'CASCADE',
+});
 
 module.exports = Seance;
