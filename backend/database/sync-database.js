2c29174e65dab16236ac8528722e991cbbb6102b
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-24 12:51:15 +0300

    synchronisation database

--- backend/database/sync-database.js
+++ backend/database/sync-database.js
@@ -6,13 +6,23 @@ const CompteCollab = require('../Modele/CompteCollab');
 const RoleCollab = require('../Modele/Role');
 const PasswordResetRequest = require('../Modele/PasswordResetRequest');
 const ArchiveCollab = require('../Modele/ArchiveCollab');
+const Formation = require('../Modele/Formation');
+const CommentaireFormation = require('../Modele/CommentaireFormation');
+const DiscussionFormation= require('../Modele/discussionFormation');
+const Module = require('../Modele/Module');
+const Seance = require('../Modele/Seance');
+const ParticipantsSeance = require('../Modele/ParticipantsSeance');
 
-
+const association = require('../Modele/associationSeanceCollab');
 
 //Synchronisation de la base de donnée 
 async function syncDatabase(){
     try{
-        await sequelize.sync({force : false}); 
+        await sequelize.sync({force : true}); 
+        const { Seance, Collaborateur, ParticipantsSeance } = association;
+
+        Collaborateur.belongsToMany(Seance, { through: ParticipantsSeance });
+        Seance.belongsToMany(Collaborateur, { through: ParticipantsSeance });
         console.log('La base de donnée est synchronisée avec succès')
     }  catch (error){
         console.error('Erreur lors de la synchronisation de la base de données :', error )
