67d45721778ae3f5541e79390cf283566c78a98d
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-24 12:57:35 +0300

    forum de discussion

---
+++ backend/Modele/formation/CommentaireFormation.js
@@ -0,0 +1,42 @@
+const {DataTypes, Model} = require('sequelize');
+const sequelize = require('../database/database');
+const DiscussionFormation = require('./discussionFormation');
+const Collaborateur = require('./Collaborateur');
+
+class CommentaireFormation extends Model{}
+
+CommentaireFormation.init({
+    contenu:{
+        type:DataTypes.STRING(500),
+        allowNull: false
+    },
+    discussion:{
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        references : {
+        model : DiscussionFormation,
+        key : 'id'
+    }
+    },
+    collaborateur:{
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        references : {
+        model : Collaborateur,
+        key : 'id'
+    }
+    },
+},{
+    sequelize,
+    modelName:'CommentaireFormation'
+})
+
+CommentaireFormation.belongsTo(DiscussionFormation, {
+    foreignKey : 'discussion',
+    onDelete : 'CASCADE'
+})
+CommentaireFormation.belongsTo(Collaborateur, {
+    foreignKey : 'collaborateur'
+})
+
+module.exports= CommentaireFormation;
