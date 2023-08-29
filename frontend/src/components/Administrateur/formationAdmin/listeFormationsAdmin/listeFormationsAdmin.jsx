8252e2989e3c15d0733353f616fb963012d5bd68
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-29 14:51:49 +0300

    liste de formation côté admin

--- frontend/src/components/Administrateur/formationAdmin/listeFormationsAdmin/listeFormationsAdmin.jsx
+++ frontend/src/components/Administrateur/formationAdmin/listeFormationsAdmin/listeFormationsAdmin.jsx
@@ -6,6 +6,7 @@ const ListeFormationAdmin = () => {
   const [formations,setFormations] = useState([]);
   const[recherche,setRecherche] = useState('');
   const[formationfiltre, setFormationsfiltre] = useState([]);
+
   //Pour la pagination
   const [currentPage, setCurrentPage] = useState(1);
   // Nombre d'éléments par page 
@@ -38,62 +39,62 @@ const ListeFormationAdmin = () => {
   return (
     <>
     <div className='content'>
-            <div className="collabListes">
-            <h1 className="collabListes_title font-bold">Les formations disponibles</h1>
-            <div className="collabListes_Item">
-                <div className="search_form">
-                  <input type="text"placeholder="Rechercher une formation" value={recherche} onChange={(e)=>{setRecherche(e.target.value)}} className=""></input>
-                  <button className="search_Button"> Rechercher </button>
-                  <Link to="#" className="AddCollab_Link">Organiser une formation</Link>
-                </div>
+      <div className="collabListes">
+        <h1 className="collabListes_title font-bold">Les formations disponibles</h1>
+          <div className="collabListes_Item">
+            <div className="search_form">
+              <input type="text"placeholder="Rechercher une formation" value={recherche} onChange={(e)=>{setRecherche(e.target.value)}} className=""></input>
+              <button className="search_Button"> Rechercher </button>
+              <Link to="#" className="AddCollab_Link">Organiser une formation</Link>
             </div>
-            </div>
+          </div>
+      </div>
     </div>
     <table className="listDepartementUser_table">
-            <thead>
-                <tr>
-                    <th className="w-40">Thème</th>
-                    <th className="w-60">Description</th>
-                    <th className="w-60">Organisateur(trice)</th>
-                </tr>
-            </thead>
-            <tbody>
-                {recherche === '' || recherche === null ? (formationfiltre.map((formation)=> (
-                    <tr key={formation.id}>
-                        <td className="w-40">{formation.theme}</td>
-                        <td className="w-60">{formation.description}</td>
-                        <td className='w-60'>{formation.nomformateur} {formation.prenomformateur}</td>
-                        <td className="w-60">
-                            {/* lien '/collaborateur/id_departement' */}
-                            <button className="table_item_icon">Voir plus</button>
-                        </td>
-                    </tr>
-                ))) : (
-                  formationfiltre.filter((formations)=>formations.theme.toLowerCase().includes(recherche.toLowerCase()) || formations.description.toLowerCase().includes(recherche.toLowerCase())).map((formation) => (
-                    <tr key={formation.id}>
-                     <td className="w-40">{formation.theme}</td>
-                        <td className="w-60">{formation.description}</td>
-                        <td className='w-60'>{formation.nomformateur} {formation.prenomformateur}</td>
-                        <td className="w-60">
-                          {/* lien '/collaborateur/id_departement' */}
-                          <button className="table_item_icon">Voir plus</button>
-                        </td>
-                    </tr>
-                ))
-            )}
-            </tbody>
-        </table>
-        <div className="list_pagination">
-            {Array.from({length : totalPages}, (_, index) => index + 1).map((page) => (
-                <button
-                    key={page}
-                    className={page === currentPage ? "active" : ""}
-                    onClick={() => handlePageChange(page)}
-                >
-                    {page}
-                </button>
+      <thead>
+        <tr>
+          <th className="w-40">Thème</th>
+          <th className="w-60">Description</th>
+          <th className="w-60">Organisateur(trice)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {recherche === '' || recherche === null ? (formationfiltre.map((formation)=> (
+          <tr key={formation.id}>
+            <td className="w-40">{formation.theme}</td>
+            <td className="w-60">{formation.description}</td>
+            <td className='w-60'>{formation.nomformateur} {formation.prenomformateur}</td>
+            <td className="w-60">
+              {/* lien '/collaborateur/id_departement' */}
+              <button className="table_item_icon">Voir plus</button>
+            </td>
+            </tr>
+        ))) : (
+        formationfiltre.filter((formations)=>formations.theme.toLowerCase().includes(recherche.toLowerCase()) || formations.description.toLowerCase().includes(recherche.toLowerCase())).map((formation) => (
+            <tr key={formation.id}>
+              <td className="w-40">{formation.theme}</td>
+              <td className="w-60">{formation.description}</td>
+              <td className='w-60'>{formation.nomformateur} {formation.prenomformateur}</td>
+              <td className="w-60">
+                {/* lien '/collaborateur/id_departement' */}
+                <button className="table_item_icon">Voir plus</button>
+              </td>
+            </tr>
+        ))
+        )}
+      </tbody>
+    </table>
+    <div className="list_pagination">
+        {Array.from({length : totalPages}, (_, index) => index + 1).map((page) => (
+          <button
+            key={page}
+            className={page === currentPage ? "active" : ""}
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </button>
             ))}
-        </div>
+    </div>
     </>
   )
 }
