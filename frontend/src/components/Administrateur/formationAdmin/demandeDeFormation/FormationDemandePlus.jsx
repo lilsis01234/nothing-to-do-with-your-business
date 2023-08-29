3ee3413d46f7761f8f44bf536b698675acfb8b9b
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-29 10:45:25 +0300

    voir plus informations

---
+++ frontend/src/components/Administrateur/formationAdmin/demandeDeFormation/FormationDemandePlus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormationDemandePlus = () => {
+  return (
+    <div>
+        <h1>Formation</h1>
+    </div>
+  )
+}
+
+export default FormationDemandePlus
---
+++ frontend/src/components/Administrateur/formationAdmin/listeFormationsAdmin/FormationPlus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormationPlus = () => {
+  return (
+    <div>
+        <h1>Formation</h1>
+    </div>
+  )
+}
+
+export default FormationPlus
