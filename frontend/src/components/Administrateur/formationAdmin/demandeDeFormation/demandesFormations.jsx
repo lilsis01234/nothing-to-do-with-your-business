4c8cfb5af606fc0b89dbf942b78f7fc42ae6803c
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-29 09:17:02 +0300

    demandes de formation

--- frontend/src/components/Administrateur/formationAdmin/demandeDeFormation/demandesFormations.jsx
+++ frontend/src/components/Administrateur/formationAdmin/demandeDeFormation/demandesFormations.jsx
@@ -5,11 +5,14 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import ListeFormationAdmin from '../listeFormationsAdmin/listeFormationsAdmin'
 import './demandeFormation.css'
+import axios from 'axios'
 
 const DemandeFormation = () => {
 
   const navigate = useNavigate();
   const [listevisible,setVisible] = useState(false);
+  const[demandeFormation,setDemandeFormation] = useState([]);
+  const [recherche,setRecherche]= useState(null);
 
   useEffect(() => {
     // const token = Cookies.get('jwt');
@@ -24,23 +27,84 @@ const DemandeFormation = () => {
         navigate('/home');
     }
 
-}, [navigate])
+    }, [navigate])
+
+  const fetchCollaborateur = () => {
+    axios.get('http://localhost:8000/api/formation/all_demandes_formations')
+      .then(res => {setDemandeFormation(res.data)})
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchCollaborateur();
+  }, [])
+  console.log(demandeFormation)
 
 const Demandes = ()=>{
   return (
+    <>
     <center>
     <div className='content'>
       <div className='collabListes'>
             <h1 className="collabListes_title font-bold">Liste des demandes de formations</h1>
             <div className="collabListes_Item">
                 <div className="search_form">
-                  <input type="text"placeholder="Rechercher une formation" className=""></input>
+                  <input type="text"placeholder="Rechercher une formation" value={recherche} onChange={(e)=>{setRecherche(e.target.value)}} className=""></input>
                   <button className="search_Button"> Rechercher </button>
                 </div>
             </div>
-      </div>
+    </div>
     </div>
     </center>
+      <table className="listDemandeFormation">
+            <thead>
+                <tr>
+                    <th >Thème</th>
+                    <th >Description</th>
+                    <th >Organisateur(trice)</th>
+                    <th >Voir plus</th>
+                    <th >Approuver</th>
+                    <th >Désapprouver</th>
+                </tr>
+            </thead>
+            <tbody>
+                {recherche === '' || recherche === null ? (demandeFormation.map((formation)=> (
+                    <tr key={formation.id}>
+                        <td >{formation.theme}</td>
+                        <td >{formation.description}</td>
+                        <td >{formation.nomformateur} {formation.prenomformateur}</td>
+                        <td ><button className="table_item_icon">Voir plus</button></td>
+                        <td >
+                            {/* lien '/collaborateur/id_departement' */}
+                            <button className="table_item_icon">Approuver</button>
+                        </td>
+                        <td >
+                            {/* lien '/delete id' */}
+                            <button className="table_item_icon">Désapprouver</button>
+                        </td>
+                    </tr>
+                ))) : (
+                  demandeFormation.filter((formations)=>formations.theme.toLowerCase().includes(recherche.toLowerCase()) || formations.description.toLowerCase().includes(recherche.toLowerCase())).map((formation) => (
+                    <tr key={formation.id}>
+                     <td >{formation.theme}</td>
+                        <td >{formation.description}</td>
+                        <td >{formation.nomformateur} {formation.prenomformateur}</td>
+                        {/* lien collaborateur id voir plus */}
+                        <td><button className="table_item_icon">Voir plus</button></td>
+                        <td >
+                            {/* lien '/collaborateur/id_departement' */}
+                            <button className="table_item_icon">Approuver</button>
+                        </td>
+                        <td >
+                            {/* lien '/delete id' */}
+                            <button className="table_item_icon">Désapprouver</button>
+                        </td>
+                    </tr>
+                ))
+            )}
+            </tbody>
+        </table>
+    </>
     )
 
 }
