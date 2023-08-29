dc1fbeaf423b1b5a5b1431e0aa0b0b182f55ab67
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-29 11:01:36 +0300

    controller module

--- backend/routes/formation/module.js
+++ backend/routes/formation/module.js
@@ -2,13 +2,18 @@ const router = require('express').Router();
 const cookieParser = require('cookie-parser');
 const Formation = require('../../Modele/formation/Formation');
 const Module = require('../../Modele/formation/Module');
+const Collaborateur = require('../../Modele/Collaborateur');
 router.use(cookieParser());
 
 router.get('/all_modules', async(req,res) => {
     Module.findAll({
         include: {
             model: Formation,
-            attributes: ['theme','id']
+            attributes: ['theme','id'],
+            include:{
+                model:Collaborateur,
+                attributes:['nom','prenom'],
+            }
         },
     })
     .then((module) => {
@@ -20,6 +25,8 @@ router.get('/all_modules', async(req,res) => {
                     description : module.description,
                     formation: module.Formation.theme,
                     idFormation: module.Formation.id,
+                    nomOrganisateur: module.Formation.Collaborateur.nom,
+                    prenomOrganisateur: module.Formation.Collaborateur.prenom,
                 }
             })
         )
