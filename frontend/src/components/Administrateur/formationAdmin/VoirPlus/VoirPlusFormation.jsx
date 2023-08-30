ac18eaccedb84a9d6742bbc25eb5fabcbea26558
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-30 16:24:00 +0300

    voir plus formation

--- frontend/src/components/Administrateur/formationAdmin/VoirPlus/VoirPlusFormation.jsx
+++ frontend/src/components/Administrateur/formationAdmin/VoirPlus/VoirPlusFormation.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const VoirPlusFormation = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // const token = Cookies.get('jwt');
+    const token = localStorage.getItem('jwt');
+    console.log(token);
+    if (!token){
+        navigate('/');
+    }
+
+    const role = localStorage.getItem('role'); 
+    if (!(role === "Administrateur")){
+        navigate('/home');
+    }
+    }, [navigate])
+    
   return (
     <div>VoirPlusFormation</div>
   )
