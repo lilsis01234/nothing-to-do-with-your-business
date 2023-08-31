7182b547e4e18ac35fd5caa765e67b7ef46c23ef
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-08-31 10:16:50 +0300

    mes formations côté user

--- frontend/src/components/User/formationsUser/mesFormations/MesFormationsUser.jsx
+++ frontend/src/components/User/formationsUser/mesFormations/MesFormationsUser.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const MesFormationsUser = () => {
     // maka localStorage.getItem('id')
+  const {id} = useParams();
   return (
     <div>
         <h1>Toutes vos formations</h1>
+        {id}
     </div>
   )
 }
